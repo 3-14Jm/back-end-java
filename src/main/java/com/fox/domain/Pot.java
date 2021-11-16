be5477ebcdd7c8f32cc82161029a65d07f43fd2c
YaroslavLys <mryaroslav337@gmail.com>
2021-11-16 12:10:44 +0200

    Added entity class for Pot

---
+++ src/main/java/com/fox/domain/Pot.java
@@ -0,0 +1,72 @@
+package com.fox.domain;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Objects;
+
+@Entity
+public class Pot {
+    private Integer id;
+    private String name;
+    private String description;
+    private Collection<Review> reviewsById;
+
+    @Id
+    @Column(name = "id")
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "description")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Pot pot = (Pot) o;
+
+        if (!Objects.equals(id, pot.id)) return false;
+        if (!Objects.equals(name, pot.name)) return false;
+        return Objects.equals(description, pot.description);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+
+    @OneToMany(mappedBy = "potByPotId")
+    public Collection<Review> getReviewsById() {
+        return reviewsById;
+    }
+
+    public void setReviewsById(Collection<Review> reviewsById) {
+        this.reviewsById = reviewsById;
+    }
+}
